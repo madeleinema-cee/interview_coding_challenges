0fb6a09d84ef81cadae75420294e99f831b35cb4
Madeleine Ma <63205888+mayue3081249@users.noreply.github.com>
2021-05-03 12:15:27 -0400

    Add files via upload

---
+++ data_visualization_with_plotly/company_headcount/company_headcount/views.py
@@ -0,0 +1,20 @@
+import json
+from django.shortcuts import render
+
+from retrieve_company_data import RetrieveCompanyData
+
+
+def home(request):
+    r = RetrieveCompanyData()
+    months, headcounts = r.main()
+
+    companyData = {
+        "months": months,
+        "headcounts": headcounts
+    }
+
+    context = {
+        'companyData': json.dumps(companyData),
+    }
+
+    return render(request, 'index.html', context)
